ac3cd6d696da624f9c5c062f7210fd80c31f6b1b
onmax <maximogarciamtnez@gmail.com>
2025-09-01 14:55:17 +0200

    setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import wasm from 'vite-plugin-wasm'
 import { defineConfig } from 'vite'
+import topLevelAwait from 'vite-plugin-top-level-await'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), wasm()],
+  plugins: [wasm(), topLevelAwait(), vue()],
   worker: {
-    plugins: () => [wasm()]
+    plugins: () => [wasm(), topLevelAwait()]
   },
   optimizeDeps: {
     exclude: ['@nimiq/core'],
