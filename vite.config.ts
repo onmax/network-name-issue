5bf6128092f6dc71b0ec65297afa05cbba16e8ec
onmax <maximogarciamtnez@gmail.com>
2025-09-01 14:50:25 +0200

    Remove conflicting Vite plugins causing build errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,9 @@
-import topLevelAwait from 'vite-plugin-top-level-await'
-import wasm from 'vite-plugin-wasm'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    wasm(),
-    topLevelAwait(),
-  ],
-  worker: {
-    plugins: () => [
-      wasm(),
-      topLevelAwait(),
-    ]
-  },
-
+  plugins: [vue()],
   optimizeDeps: {
     exclude: ['@nimiq/core'],
   },
