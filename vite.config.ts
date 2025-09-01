fff2ad95d6a70dc890adc325c9d05f128182feb1
onmax <maximogarciamtnez@gmail.com>
2025-09-01 14:53:59 +0200

    asd

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), wasm()],
+  worker: {
+    plugins: () => [wasm()]
+  },
   optimizeDeps: {
     exclude: ['@nimiq/core'],
   },
